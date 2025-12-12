eb77dc79e701378f48abbc3705011398bd90581f
Diego Delvig <157409019+DiegoDelvig@users.noreply.github.com>
2025-12-12 09:54:57 +0100

    Add files via upload

--- main.sh
+++ main.sh
@@ -10,20 +10,13 @@ if [ "$1" == "histo" ] && [ "$2" == "src" ]; then
     cut -d';' -f1 "$TEMP_FILE" > tests/histo_nom.txt
     cut -d';' -f2 "$TEMP_FILE" > tests/histo_quantite.txt
 
-    # Supprime le fichier temporaire
     rm "$TEMP_FILE"
 
     # On compile le fichier histo.c puis on l'execute
     make src
     ./c-wildwater
 
-    # On affiche les fichiers créer par le fichier C
-    echo " "
-    echo "--- 📊 Résultats de l'histogramme (ID USINE | VOLUME TOTAL CAPTÉ) ---"
-    # L'option awk pour un affichage bien aligné
-    paste -d'|' tests/histo_nom_result.txt tests/histo_quantite_result.txt 
-    echo "------------------------------------------------------------------"
-    
+    #GNU Plot 
     
 elif [ "$1" == "histo" ] && [ "$2" == "real" ]; then
     TEMP_FILE="tests/temp.dat"
@@ -34,9 +27,24 @@ elif [ "$1" == "histo" ] && [ "$2" == "real" ]; then
     cut -d';' -f2 "$TEMP_FILE" > tests/histo_quantite.txt
     cut -d';' -f3 "$TEMP_FILE" > tests/histo_fuite.txt
 
-    # Nettoyage
     rm "$TEMP_FILE"
 
+    # Gnu plot
+
+elif [ "$1" == "histo" ] && [ "$2" == "max" ]; then
+    FILE_OUT="tests/vol_max.dat"
+
+    DATA=$(awk -F';' '$1 == "-" && $3 == "-" && $5 == "-"' data/c-wildwater_v0.dat | cut -d';' -f2,4 | tr '[:lower:]' '[:upper:]')
+    
+    TOP=$(echo "$DATA" | sort -t';' -k2nr | head -n 3)
+    BOTTOM=$(echo "$DATA" | sort -t';' -k2n | head -n 3)
+    echo "identifier;max volume (k.m³.year⁻¹)"
+    echo "$TOP"
+    
+    echo ""
+    echo "identifier;max volume (k.m³.year⁻¹)"
+    echo "$BOTTOM"        
+    
 else
     echo "Cette commande n'est pas reconnue."
 fi
